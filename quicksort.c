b26ee94f2589f3936c787ff1b6bf5beb9d2d2310
HsinYuHsiao <HsinYuHsiao@users.noreply.github.com>
2021-03-02 20:45:27 +0800

    Fix arrayPartition when nums[pivot] <= nums[upBound]

--- quicksort.c
+++ quicksort.c
@@ -32,9 +32,22 @@ int arrayPartition(int *nums, int start, int end)
 		}
 	}
 
+	if (pivot == upBound)
+	{
+		//nums[pivot] is the smallest in the array
+		return upBound;
+	}
 	if (nums[pivot] > nums[upBound])
 	{
 		swap(&nums[pivot], &nums[upBound]);
+		return upBound;
+	}
+	if (pivot != lowBound - 1 && nums[pivot] <= nums[lowBound])
+	{
+		//swap pivot to the right of the left partition
+		//otherwise the array would not be partitioned correctly
+		swap(&nums[pivot], &nums[lowBound - 1]);
+		return lowBound - 1;
 	}
 	return upBound;
 }
@@ -53,8 +66,10 @@ int main()
 {
 	int data[] = {9, 4, 19, 21, 7, 10, 6, 1, 2};
 	int data2[] = {1, 4, 7, 90, 9};
+	int data3[] = {-1429, -9671, 3622, 581, -2419, -996};
 	size_t sizeData = sizeof(data) / sizeof(data[0]);
 	size_t sizeData2 = sizeof(data2) / sizeof(data2[0]);
+	size_t sizeData3 = sizeof(data3) / sizeof(data3[0]);
 
 	printf("data1 before : ");
 	for (int i = 0; i < sizeData; i++)
@@ -78,6 +93,18 @@ int main()
 	for (int i = 0; i < sizeData2; i++)
 	{
 		printf("%d ", data2[i]);
+	}
+
+	printf("\ndata3 before : ");
+	for (int i = 0; i < sizeData3; i++)
+	{
+		printf("%d ", data3[i]);
+	}
+	quickSort(data3, 0, sizeData3 - 1);
+	printf("\ndata3 after ");
+	for (int i = 0; i < sizeData3; i++)
+	{
+		printf("%d ", data3[i]);
 	}
 	return EXIT_SUCCESS;
 }
